ab68e6e74829ca000d666745cb7c2aeffe2d7496
Ganesh Chincholkar <ganesh.chincholkar@siemens.com>
2024-08-22 17:58:45 +0530

    server started message

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,10 @@ const WebSocket = require('ws');
 
 const webSocketServer = new WebSocket.Server({ port: 8080 });
 
+webSocketServer.on('listening', () => {
+    console.log('WebSocket server is listening on port 8080');
+});
+
 webSocketServer.on('connection', (webSocket) => {
     console.log('User connected');
 
