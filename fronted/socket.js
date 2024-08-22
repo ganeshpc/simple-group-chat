ec72af1b1635ad01483d2328c773d227a0b8a4c4
Ganesh Chincholkar <ganesh.chincholkar@siemens.com>
2024-08-22 17:58:25 +0530

    add message to list

--- fronted/socket.js
+++ fronted/socket.js
@@ -2,8 +2,11 @@ const webSocket = new WebSocket('ws://localhost:8080');
 
 webSocket.onmessage = (socketMessage) => {
     console.log(socketMessage.data);
+    const messageList = document.querySelector('ul');
+    messageList.innerHTML += `<li>${socketMessage.data}</li>`;
 };
 
 document.querySelector('button').addEventListener('click', () => {
-    webSocket.send('Hello, server!');
+    const messageText = document.querySelector('input').value;
+    webSocket.send(messageText);
 });
