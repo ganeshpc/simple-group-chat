d3d06eb8ee4505ad83e0e49620e62d7379d9960e
Ganesh Chincholkar <ganesh.chincholkar@siemens.com>
2024-08-22 17:38:49 +0530

    working socket server

--- backend/index.js
+++ backend/index.js
@@ -6,6 +6,7 @@ webSocketServer.on('connection', (webSocket) => {
     console.log('User connected');
 
     webSocket.on('message', (message) => {
+        console.log(message);
         webSocket.send(message);
     });
 });
